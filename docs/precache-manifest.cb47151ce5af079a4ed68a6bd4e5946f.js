3c8c509c2a348f8cde1cecacfa2e73a5c89317b8
Joseph Jimenez <jajm2797@gmail.com>
2020-10-23 18:48:32 -0400

    Docs Agregado

---
+++ docs/precache-manifest.cb47151ce5af079a4ed68a6bd4e5946f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24310777ffa79918b6ca1f8da6cb835",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd81ed304713ef6d572c",
+    "url": "/static/css/2.da1bc806.chunk.css"
+  },
+  {
+    "revision": "8fa15d19081660c82d24",
+    "url": "/static/css/main.ec5786bf.chunk.css"
+  },
+  {
+    "revision": "cd81ed304713ef6d572c",
+    "url": "/static/js/2.df0e6ea6.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.df0e6ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa15d19081660c82d24",
+    "url": "/static/js/main.a0c74590.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
